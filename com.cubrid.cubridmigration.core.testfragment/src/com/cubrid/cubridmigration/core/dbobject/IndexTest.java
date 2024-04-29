6fef1296cba64b7ca88ac1e9a122fd3b1b679dd4
Dongmin Kim <49967405+Srltas@users.noreply.github.com>
2024-04-29 09:27:38 +0900

    [TOOLS-4582] Change source code license (#190)
    http://jira.cubrid.org/browse/TOOLS-4582

--- com.cubrid.cubridmigration.core.testfragment/src/com/cubrid/cubridmigration/core/dbobject/IndexTest.java
+++ com.cubrid.cubridmigration.core.testfragment/src/com/cubrid/cubridmigration/core/dbobject/IndexTest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2009 Search Solution Corporation. All rights reserved by Search Solution.
+ * Copyright (C) 2008 Search Solution Corporation.
+ * Copyright (C) 2016 CUBRID Corporation.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
@@ -26,7 +27,8 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
  * OF SUCH DAMAGE.
  *
- */ package com.cubrid.cubridmigration.core.dbobject;
+ */
+package com.cubrid.cubridmigration.core.dbobject;
 
 import org.junit.Test;
 
